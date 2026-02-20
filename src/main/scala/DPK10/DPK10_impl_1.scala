6895575cd909e03b9e7488105cf04fddd2eb6bae
FFucks <fabio_geiss@hotmail.com>
2026-02-19 23:12:10 -0300

    added DPK11

---
+++ src/main/scala/DPK10/DPK10_impl_1.scala
@@ -0,0 +1,11 @@
+package DPK10
+
+class DPK10_impl_1 {
+
+}
+
+@main def dpk10Impl1(): Unit = {
+
+    val dpk08 = new DPK10_impl_1
+    
+}
---
+++ src/main/scala/DPK11/DPK11_impl_1.scala
@@ -0,0 +1,12 @@
+package DPK11
+
+class DPK11_impl_1 {
+
+}
+
+@main def dpk11Impl1(): Unit = {
+
+    val dpk11 = new DPK11_impl_1
+    
+
+}
