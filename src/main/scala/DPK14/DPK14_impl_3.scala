7788437ac77881ff7250229b9b55e996dae16ccc
FFucks <fabio_geiss@hotmail.com>
2026-02-27 01:20:06 -0300

    DPK14_impl_3

---
+++ src/main/scala/DPK14/DPK14_impl_3.scala
@@ -0,0 +1,73 @@
+package DPK14
+
+/*
+Create a function that can move a fighter in a 2D grid. The grid should be a 2D array.
+grid = [
+  ["Ryu", "E.Honda", "Blanka", "Guile", "Balrog", "Vega"],
+  ["Ken", "Chun Li", "Zangief", "Dhalsim", "Sagat", "M.Bison"]
+]
+The moves should be:
+up
+down
+left
+right
+The function should receive the grid, the initial position of the fighter and a list of moves.
+move(grid, [0,0], ["up", "left", "down", "right"])
+Every time a player move to a new position, the old position should be empty.
+move(grid, [0,0], ["up"]) ->
+      ["", "E.Honda", "Blanka", "Guile", "Balrog", "Vega",
+      "Ryu", "Chun Li", "Zangief", "Dhalsim", "Sagat", "M.Bison"]
+What the function needs to return is a list with the name of all players was beaten, therefore removed from the grid.
+move(grid, [0,0], ["up", "left", "down", "right"]) -> ["ken", "M.Bison", "Vega"]
+
+*/
+
+class DPK14_impl_3 {
+
+    def move(grid: Array[Array[String]], position: (Int, Int), moves: List[String]): List[String] = {
+        var beaten = List.empty[String]
+        var row = position._1
+        var col = position._2
+        val fighter = grid(row)(col)
+
+        def updatePosition(move: String): Unit = {
+            move match {
+                case "up" => row = (row - 1 + grid.length) % grid.length
+                case "down" => row = (row + 1) % grid.length
+                case "left" => col = (col - 1 + grid(row).length) % grid(row).length
+                case "right" => col = (col + 1) % grid(row).length
+                case _ => ()
+            }
+        }
+
+        moves.foreach { move =>
+            grid(row)(col) = ""
+            updatePosition(move)
+
+            val opponent = grid(row)(col)
+            if (opponent != "") {
+                beaten = beaten :+ opponent
+            }
+
+            grid(row)(col) = fighter
+        }
+
+        beaten
+    }
+}
+
+@main def Dpk14Impl3(): Unit = {
+
+    val dpk14 = new DPK14_impl_3
+
+    val grid: Array[Array[String]] = Array(
+        Array("Ryu", "E.Honda", "Blanka", "Guile", "Balrog", "Vega"),
+        Array("Ken", "Chun Li", "Zangief", "Dhalsim", "Sagat", "M.Bison")
+    )
+
+    val position = (0, 0)
+
+    val moves: List[String] = List("up", "left", "down", "right")
+
+    println(dpk14.move(grid, position, moves))
+}
