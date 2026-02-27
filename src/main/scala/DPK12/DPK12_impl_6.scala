360447b40189582543f618cc25ba95f719255d93
FFucks <fabio_geiss@hotmail.com>
2026-02-27 03:32:32 -0300

    DPK12_impl_6

---
+++ src/main/scala/DPK12/DPK12_impl_6.scala
@@ -0,0 +1,44 @@
+package DPK12
+
+/*DPK12 Sort
+    Create a function that can sort a list of numbers.The implementation should be a Bubble Sort(good to learn but terrible to use in production).
+
+   bubble_sort([5, 4, 3, 2, 1] ) ->[1, 2, 3, 4, 5]
+The sort function should receive a collection(list or array) and return a new sorted list
+.*/
+
+class DPK12_impl_6 {
+
+    def bubble_sort(list: Seq[Int]): List[Int] = {
+        val array = list.toArray
+        var swapped = true
+        var length = array.length
+
+        while (swapped) {
+            swapped = false
+
+            for (index <- 0 until length - 1) {
+                if (array(index) > array(index + 1)) {
+                    val temp = array(index)
+                    array(index) = array(index + 1)
+                    array(index + 1) = temp
+                    swapped = true
+                }
+            }
+
+            length -= 1
+        }
+
+        array.toList
+    }
+}
+
+@main def dpk12Impl6(): Unit = {
+    val arr = Array(10, 9, 8, 7, 6)
+    val list = List(5, 4, 3, 2, 1)
+
+    val dpk12 = new DPK12_impl_6
+
+    println(dpk12.bubble_sort(arr))
+    println(dpk12.bubble_sort(list))
+}
