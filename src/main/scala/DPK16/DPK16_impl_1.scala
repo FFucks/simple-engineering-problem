c1109584b531e9007e9a0f68c720672a04a6c50e
FFucks <fabio_geiss@hotmail.com>
2026-03-06 10:29:39 -0300

    DPK16

--- src/main/scala/DPK16/DPK16_impl_1.scala
+++ src/main/scala/DPK16/DPK16_impl_1.scala
@@ -1,8 +1,10 @@
 package DPK16
 
-import scala.util.Random
-
 class DPK16_impl_1
+
+import scala.util.Random
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
 
 class Mosquito(var row: Int, var col: Int) {
 
@@ -10,16 +12,57 @@ class Mosquito(var row: Int, var col: Int) {
         row = Random.nextInt(100)
         col = Random.nextInt(100)
     }
+
+    override def toString: String = s"Mosquito($row,$col)"
 }
 
 class Exterminator(var row: Int, var col: Int) {
 
-    def moveTo(newRow: Int, newCol: Int) = {
+    def moveTo(newRow: Int, newCol: Int): Unit = {
         row = newRow
         col = newCol
     }
+
+    override def toString: String = s"Exterminator($row,$col)"
 }
 
-class Game() {
+class Game {
 
+    val size = 100
+
+    val matrix: Array[Array[String]] =
+        Array.fill(size, size)(".")
+
+    val mosquitoes: List[Mosquito] = List.fill(10)(new Mosquito(Random.nextInt(100), Random.nextInt(100)))
+
+    val exterminator = new Exterminator(50, 50)
+
+
+    def printMatrix(): Unit = {
+        matrix.foreach(row => println(row.mkString(" ")))
+    }
+
+    def start(): Unit = {
+
+        Future {
+            while (true) {
+                mosquitoes.foreach(_.move())
+                exterminator.moveTo(Random.nextInt(100), Random.nextInt(100))
+
+                println("------ GAME MOSQUITO ------")
+                printMatrix()
+
+                Thread.sleep(1000)
+            }
+        }
+    }
+}
+
+@main def mosquitoGame(): Unit = {
+
+    val game = new Game()
+
+    game.start()
+
+    Thread.sleep(Long.MaxValue)
 }
