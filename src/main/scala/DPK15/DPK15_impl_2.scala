c728c6e2f888a199988c354b6da015cd572a923b
FFucks <fabio_geiss@hotmail.com>
2026-03-05 23:49:35 -0300

    DPK15

--- src/main/scala/DPK15/DPK15_impl_2.scala
+++ src/main/scala/DPK15/DPK15_impl_2.scala
@@ -2,17 +2,128 @@ package DPK15
 
 class DPK15_impl_2
 
-/*
 @main def dpk15Impl2(): Unit = {
 
-    val person = new Person("John", 30, List())
+    val john = new PersonImpl2("John", 30)
+    val paul = new PersonImpl2("Paul", 35)
+    val george = new PersonImpl2("George", 28)
 
-    person.addFriend("Paul")
-    person.addFriend("George")
-    person.addFriend("Ringo")
+    john.addFriend("Paul")
+    john.addFriend("George")
 
-    println(person.getFriends)
-    println(person.getAge)
-    println(person.getName)
+    paul.addFriend("John")
+
+    george.addFriend("John")
+    george.addFriend("Paul")
+    george.addFriend("Ringo")
+
+    val people = List(john, paul, george)
+
+    val service = new PersonServiceImpl2(people)
+
+    println(service.personWithMostFriends().getName())
+    println(service.personWithLeastFriends().getName())
+    println(service.personWithOldestFriend().getName())
 }
-*/
+
+class PersonImpl2 (name: String, age: Int) {
+
+    private val friendsList = new FriendImpl2()
+
+    def addFriend(name: String): Unit = {
+        friendsList.addFriend(name)
+    }
+
+    def removeFriend(name: String): Unit = {
+        friendsList.removeFriend(name)
+    }
+
+    def getFriends(): List[String] = {
+        friendsList.getFriends()
+    }
+
+    def getFriendsCount(): Int = {
+        friendsList.getFriends().size
+    }
+
+    def getAge(): Int = {
+        age
+    }
+
+    def getName(): String = {
+        name
+    }
+}
+
+class FriendImpl2 {
+
+    private var friends: List[String] = List()
+
+    def addFriend(name: String): Unit = {
+        if (!friends.contains(name)) {
+            friends = friends :+ name
+        }
+    }
+
+    def removeFriend(name: String): Unit = {
+        friends = friends.filter(_ != name)
+    }
+
+    def getFriends(): List[String] = {
+        friends
+    }
+}
+
+class PersonServiceImpl2(private val people: List[PersonImpl2]) {
+
+    def personWithMostFriends(): PersonImpl2 = {
+        var result = people.head
+        var maxFriends = result.getFriendsCount()
+
+        people.foreach { person =>
+            val count = person.getFriendsCount()
+
+            if (count > maxFriends) {
+                maxFriends = count
+                result = person
+            }
+        }
+
+        result
+    }
+
+    def personWithLeastFriends(): PersonImpl2 = {
+        var result = people.head
+        var minFriends = result.getFriendsCount()
+
+        people.foreach { person =>
+            val count = person.getFriendsCount()
+
+            if (count < minFriends) {
+                minFriends = count
+                result = person
+            }
+        }
+
+        result
+    }
+
+    def personWithOldestFriend(): PersonImpl2 = {
+        var result: PersonImpl2 = people.head
+        var oldestAge: Int = 0
+
+        for (person <- people) {
+            for (friendName <- person.getFriends()) {
+                for (p <- people) {
+                    if (p.getName() == friendName) {
+                        if (p.getAge() > oldestAge) {
+                            oldestAge = p.getAge()
+                            result = person
+                        }
+                    }
+                }
+            }
+        }
+        result
+    }
+}
---
+++ src/main/scala/DPK16/DPK16_impl_1.scala
@@ -0,0 +1,15 @@
+package DPK16
+
+class DPK16_impl_1
+
+class Exterminator() {
+
+}
+
+class Mosquito() {
+
+}
+
+class Game() {
+
+}
