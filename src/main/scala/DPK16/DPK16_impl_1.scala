1488bd36bdf9e1484ab4d190956b8e3424e4e387
FFucks <fabio_geiss@hotmail.com>
2026-03-06 01:18:28 -0300

    DPK16

--- src/main/scala/DPK16/DPK16_impl_1.scala
+++ src/main/scala/DPK16/DPK16_impl_1.scala
@@ -1,13 +1,23 @@
 package DPK16
 
+import scala.util.Random
+
 class DPK16_impl_1
 
-class Exterminator() {
+class Mosquito(var row: Int, var col: Int) {
 
+    def move(): Unit = {
+        row = Random.nextInt(100)
+        col = Random.nextInt(100)
+    }
 }
 
-class Mosquito() {
+class Exterminator(var row: Int, var col: Int) {
 
+    def moveTo(newRow: Int, newCol: Int) = {
+        row = newRow
+        col = newCol
+    }
 }
 
 class Game() {
